c498e5d6329fa7fd01a0fc0d7e03ff95483ff227
Iamabdul <a.alhaj@hotmail.co.uk>
2025-07-27 20:40:43 +0100

    if window is not undefined

--- lib/gtagHelper.ts
+++ lib/gtagHelper.ts
@@ -1,5 +1,7 @@
-export const pageview = (GA_MEASUREMENT_ID : string, url : string) => {
+export const pageview = (GA_MEASUREMENT_ID: string, url: string) => {
+  if (typeof window !== "undefined") {
     window.gtag("config", GA_MEASUREMENT_ID, {
-        page_path: url,
+      page_path: url,
     });
+  }
 };
